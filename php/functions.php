27d0b7514dcc2809041c05b6d93c78009edf8c1d
Benson Ho <benson@goodafternoon.com.au>
2017-03-29 16:58:56 +0900

    fixing file naming conventions
    using dashes instead of underscores

--- php/functions.php
+++ php/functions.php
@@ -9,15 +9,15 @@
 // Initial configurations of the wordpress theme.
 require_once( 'functions/utilities.php' );
 require_once( 'functions/reset.php' );
-require_once( 'functions/wp_admin.php' );
+require_once( 'functions/wp-admin.php' );
 require_once( 'functions/disable.php' );
 
 // Helper functions.
 require_once( 'helpers/html.php' );
 require_once( 'helpers/post.php' );
-require_once( 'helpers/advanced_custom_fields.php' );
-require_once( 'helpers/slider_gallery.php' );
-require_once( 'helpers/bem_nav_menu.php' );
+require_once( 'helpers/advanced-custom-fields.php' );
+require_once( 'helpers/slider-gallery.php' );
+require_once( 'helpers/bem-nav-menu.php' );
 
 // Theme specific functionality.
 require_once( 'theme/theme.php' );
