72a07a549c2c495a154eb77b55e1db760365f994
Benson Ho <benson@goodafternoon.com.au>
2017-03-29 16:01:53 +0900

    using dash instead of underscore

--- php/functions/wp_admin.php
+++ php/functions/wp_admin.php
@@ -10,7 +10,7 @@
  * @ignore
  */
 function load_custom_wp_admin_style() {
-  wp_register_style("custom_wp_admin_css", get_template_directory_uri() . "/wp_admin.css", false, "1.0.0");
+  wp_register_style("custom_wp_admin_css", get_template_directory_uri() . "/wp-admin.css", false, "1.0.0");
   wp_enqueue_style("custom_wp_admin_css");
 }
 add_action("admin_enqueue_scripts", "load_custom_wp_admin_style");
