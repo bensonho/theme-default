f692c4027353231b3ef5f1e86f2c918635893825
Benson Ho <benson@goodafternoon.com.au>
2017-04-11 19:08:46 +0900

    adding some comments and structuring this extended class a bit better

--- php/functions/class.bem-nav-menu.php
+++ php/functions/class.bem-nav-menu.php
@@ -1,80 +1,103 @@
 <?php
 /**
+ * Overriding the default wordpress menu to provide BEM style class names.
  *
- *
- * @package helpers/bem-nav-menu
- */
-
-
-/**
- * Overriding the default wordpress menu to provide BEM style class names
+ * @package functions/bem-nav-menu
  */
 class BEM_Nav_Menu extends Walker_Nav_Menu {
 
+	/**
+	 * Override the default behaviour of navigation links.
+	 *
+	 * @param String  $output pointer to the output of the menu.
+	 * @param Object  $item current menu item.
+	 * @param Integer $depth current depth of the item's nesting.
+	 * @param Array   $args arguments of the item.
+	 * @param Integer $id item's post id.
+	 */
 	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-		global $post;
-
-		$slug       = slugify( $item->title );
-		$block_name = ! empty( $args->menu_class ) ? $args->menu_class : '';
-
-		$selected_item = '';
-		$selected_link = '';
-
 		if ( empty( $item->classes ) ) {
 			return '';
 		}
 
+		$block_name = ! empty( $args->menu_class ) ? $args->menu_class : '';
+		$item_class = reset( $item->classes );
+		$item_slug  = '';
+		$item_title = apply_filters( 'the_title', $item->title, $item->ID );
+		$link_class = '';
+		$link_slug  = '';
+
+		// Add the selected class.
 		if ( array_search( 'current-menu-item', $item->classes ) ) {
-			$selected_item = "$block_name-item--selected";
-			$selected_link = "$block_name-link--selected";
+			$item_class .= " $block_name-item--selected";
+			$link_class .= " $block_name-link--selected";
 		}
 
+		// Add the parent selected class.
 		if ( array_search( 'current-menu-parent', $item->classes ) ) {
-			$selected_item .= " $block_name-item-parent--selected";
-			$selected_link .= " $block_name-link-parent--selected";
+			$item_class .= " $block_name-item-parent--selected";
+			$link_class .= " $block_name-link-parent--selected";
 		}
 
+		// Add the has-children class.
 		if ( array_search( 'menu-item-has-children', $item->classes ) ) {
-			$selected_item .= " $block_name-item-has-children";
-			$selected_link .= " $block_name-link-has-children";
+			$item_class .= " $block_name-item-has-children";
+			$link_class .= " $block_name-link-has-children";
 		}
 
-		$output .= "<li class='$block_name-item $block_name-item-$slug $selected_item'>";
+		if ( ! empty( $args->slug_class ) ) {
+			$item_slug .= "$block_name-item-{slugify( $item->title )}";
+			$link_slug .= "$block_name-link-{slugify( $item->title )}";
+		}
 
-		$atts = array();
-		$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
-		$atts['target'] = ! empty( $item->target )     ? $item->target     : '';
-		$atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
-		$atts['href']   = ! empty( $item->url )        ? $item->url        : '';
-		$atts['class']  = "$block_name-link $block_name-link-$slug $selected_link {$item->classes[0]}";
+		// Link attributes.
+		$attributes           = array();
+		$attributes['class']  = "$block_name-link $link_slug $link_class";
+		$attributes['href']   = ! empty( $item->url ) ? $item->url : '';
+		$attributes['rel']    = ! empty( $item->xfn ) ? $item->xfn : '';
+		$attributes['target'] = ! empty( $item->target ) ? $item->target : '';
+		$attributes['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
 
-		$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+		// Filter to allow more attributes to be added.
+		$attributes = apply_filters( 'nav_menu_link_attributes', $attributes, $item, $args, $depth );
 
-		$attributes = '';
+		// Flatten out the attributes list into a string.
+		$attributes_output = '';
 
-		foreach ( $atts as $attr => $value ) {
+		foreach ( $attributes as $attribute => $value ) {
 			if ( ! empty( $value ) ) {
-				$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
-				$attributes .= ' ' . $attr . '="' . $value . '"';
+				$value = 'href' === $attribute ? esc_url( $value ) : esc_attr( $value );
+				$attributes_output .= " $attribute='$value'";
 			}
 		}
 
-		$item_output = $args->before;
-		$item_output .= '<a' . $attributes . '>';
-		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-		$item_output .= '</a>';
-		$item_output .= $args->after;
+		$output .= "<li class='$block_name-item $item_slug $item_class'>";
 
-		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+		$link_output = "
+		{$args->before}
+		<a $attributes_output>
+			{$args->link_before}
+			$item_title
+			{$args->link_after}
+		</a>
+		{$args->after}
+		";
+
+		// Filter to add more stuff after the link.
+		$output .= apply_filters( 'walker_nav_menu_start_el', $link_output, $item, $depth, $args );
 	}
 
+	/**
+	 * Override the default behaviour of navigation child list.
+	 *
+	 * @param String  $output pointer to the output of the menu.
+	 * @param Integer $depth current depth of the item's nesting.
+	 * @param Array   $args arguments of the item.
+	 */
 	function start_lvl( &$output, $depth = 0, $args = array() ) {
-		$block_name = $args->menu_class;
+		$block_name = ! empty( $args->menu_class ) ? $args->menu_class : '';
 
 		$output .= "<ul class='$block_name-child'>";
 	}
 
-	function end_lvl( &$output, $depth = 0, $args = array() ) {
-		$output .= '</ul>';
-	}
 }
